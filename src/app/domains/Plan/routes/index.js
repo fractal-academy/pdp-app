bd20020d77973912b3a9118fd78d14bcfcbec8a8
Maxim <maxim.makarov14@gmail.com>
2021-03-05 15:21:15 +0200

    Added route to display list of plans for student

--- src/app/domains/Plan/routes/index.js
+++ src/app/domains/Plan/routes/index.js
@@ -1,4 +1,6 @@
 import { PlanCreate } from './PlanCreate'
 import { PlanEdit } from './PlanEdit'
 import { PlanShow } from './PlanShow'
-export { PlanCreate, PlanEdit, PlanShow }
+import { PlanAll } from './PlanAll'
+
+export { PlanCreate, PlanEdit, PlanShow, PlanAll }
