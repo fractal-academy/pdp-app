144a4e2cca5d670758a9c23596196857a7e0acea
Olena <olenashevchuk0@gmail.com>
2021-03-06 00:54:29 +0200

    added userAdvView

--- src/app/domains/User/components/views/UserAdvancedView/UserAdvancedView.template.js
+++ src/app/domains/User/components/views/UserAdvancedView/UserAdvancedView.template.js
@@ -1,50 +1,57 @@
 import PropTypes from 'prop-types'
+import { Avatar, Typography, Col } from 'antd'
+import { Row } from 'antd-styled'
+import { RoleSimpleView } from 'domains/Role/components/views/RoleSimpleView'
+const { Title } = Typography
 
 /**
  * @info UserAdvancedView (05 Mar 2021) // CREATION DATE
  *
  * @comment UserAdvancedView - React component.
  *
- * @since 05 Mar 2021 ( v.0.0.1 ) // LAST-EDIT DATE
+ * @since 05 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const UserAdvancedView = (props) => {
   // [INTERFACES]
-  /*
-  code sample: 
-  const { data } = props
-  */
-
-  // [ADDITIONAL_HOOKS]
-  /*
-  code sample: 
-  const firestore = useFirestore()
-  */
-
-  // [COMPONENT_STATE_HOOKS]
-  /*
-  code sample:
-  const singleton = useRef(true) // references also put here
-  const [state, setState] = useState({})
-  */
-
-  // [HELPER_FUNCTIONS]
+  const { avatarURL, firstName, secondName, email, role, avatarLeft } = props
 
   // [COMPUTED_PROPERTIES]
-  /* 
-    code sample: 
-    const userDisplayName = user.firstName + user.lastName
-  */
+  const userDisplayName =
+    firstName && secondName
+      ? `${firstName} ${secondName}`
+      : firstName
+      ? firstName
+      : secondName
+      ? secondName
+      : email
 
   // [USE_EFFECTS]
 
   // [TEMPLATE]
-  return <div>UserAdvancedView</div>
+  return (
+    <Row gutter={[16, 0]} flexDirection={!avatarLeft && 'row-reverse'}>
+      <Col>
+        <Avatar src={avatarURL} size={64} />
+      </Col>
+      <Col>
+        <Title level={4}>{userDisplayName}</Title>
+        <RoleSimpleView role={role} />
+      </Col>
+    </Row>
+  )
 }
 
 // [PROPTYPES]
-UserAdvancedView.propTypes = {}
+UserAdvancedView.propTypes = {
+  avatarURL: PropTypes.string,
+  firstName: PropTypes.string,
+  secondName: PropTypes.string,
+  email: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+  avatarLeft: PropTypes.bool
+}
 
 export default UserAdvancedView
