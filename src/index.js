8d6788ac474fdbf777727c5e110dea4aaab7f558
Maxim <maxim.makarov14@gmail.com>
2021-03-06 00:38:33 +0200

    Added theme for antd-styled

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import App from './app'
+import theme from './config/theme/antdStyled'
 import 'styles/index.less'
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Router>,
   document.getElementById('root')
 )
