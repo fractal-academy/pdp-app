9082332243e4b022a725eba9a7634cff578abfe7
Maxim <maxim.makarov14@gmail.com>
2021-03-05 17:34:04 +0200

    Added antd styles to storybook, also we can configure antd theme from now =)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,30 @@
 const cracoWebpack = require('./webpack.config')
+const antdTheme = require('../src/config/theme')
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    {
+      name: '@storybook/preset-ant-design',
+      options: {
+        lessOptions: {
+          modifyVars: antdTheme
+        }
+      }
+    },
+    {
+      "name": "@storybook/preset-create-react-app",
+      "options": {
+        "craOverrides": {
+          "fileLoaderExcludes": ["less"]
+        }
+      }
+    }
   ],
   webpackFinal:(config)=>{
     return { ...config, resolve: { ...config.resolve, alias: {...config.resolve.alias,...cracoWebpack.resolve.alias} } }
