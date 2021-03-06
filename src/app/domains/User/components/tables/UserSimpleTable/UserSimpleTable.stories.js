3298c7dccb1d7ba0baefa258264c616c8254ceb8
Olena <olenashevchuk0@gmail.com>
2021-03-06 22:15:38 +0200

    added userSimpleTable

--- src/app/domains/User/components/tables/UserSimpleTable/UserSimpleTable.stories.js
+++ src/app/domains/User/components/tables/UserSimpleTable/UserSimpleTable.stories.js
@@ -1,12 +1,85 @@
 import React from 'react'
 import UserSimpleTable from './UserSimpleTable.template'
+import { UserSimpleView } from 'domains/User/components/views'
+import { CompanySimpleView } from 'domains/Company/components/views'
+import { RoleSimpleView } from 'domains/Role/components/views'
+import { SpecialitySimpleView } from 'domains/Speciality/components/views'
+import { Typography } from 'antd'
+const { Text } = Typography
 
 const metadata = {
   title: 'app/domains/User/components/tables/UserSimpleTable',
   component: UserSimpleTable
 }
 export default metadata
+const data = [
+  {
+    key: '1',
+    idUser: 8,
+    avatarURL: '',
+    firstName: 'Fractal',
+    secondName: 'Band2',
+    email: 'email@gmail.com',
+    role: 'admin',
+    companyId: '123',
+    specialityId: '456'
+  },
+  {
+    key: '2',
+    idUser: 8,
+    avatarURL:
+      'https://firebasestorage.googleapis.com/v0/b/expenses-senseteq.appspot.com/o/photo_2020-11-27_19-32-45.jpg?alt=media&token=75958d4d-46ab-458f-b413-e81696c8c16d',
+    firstName: 'Fractal',
+    secondName: 'Band2',
+    email: 'email@gmail.com',
+    role: 'admin',
+    companyId: '123',
+    specialityId: '456'
+  }
+]
 
-export const UserSimpleTableStory = (args) => <UserSimpleTable {...args} />
+const columns = [
+  {
+    title: 'User',
+    key: 'user',
+    render: (text, data) => (
+      <UserSimpleView
+        avatarURL={data.avatarURL}
+        firstName={data.firstName}
+        secondName={data.secondName}
+        email={data.email}
+      />
+    )
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+    render: (email) => <Text type="secondary">{email}</Text>
+  },
+  {
+    title: 'Role',
+    dataIndex: 'role',
+    key: 'role',
+    render: (role) => <RoleSimpleView role={role} />
+  },
+  {
+    title: 'Company',
+    key: 'company',
+    dataIndex: 'companyId',
+    render: (companyId) => <CompanySimpleView companyId={companyId} />
+  },
+  {
+    title: 'Speciality',
+    key: 'speciality',
+    dataIndex: 'specialityId',
+    render: (specialityId) => (
+      <SpecialitySimpleView specialityId={specialityId} />
+    )
+  }
+]
+export const UserSimpleTableStory = (args) => (
+  <UserSimpleTable data={data} columns={columns} {...args} />
+)
 
 UserSimpleTableStory.args = {}
--- src/app/domains/User/components/tables/UserSimpleTable/UserSimpleTable.template.js
+++ src/app/domains/User/components/tables/UserSimpleTable/UserSimpleTable.template.js
@@ -1,50 +1,28 @@
 import PropTypes from 'prop-types'
+import { Table } from 'antd'
 
 /**
  * @info UserSimpleTable (05 Mar 2021) // CREATION DATE
  *
  * @comment UserSimpleTable - React component.
  *
- * @since 05 Mar 2021 ( v.0.0.1 ) // LAST-EDIT DATE
+ * @since 06 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const UserSimpleTable = (props) => {
   // [INTERFACES]
-  /*
-  code sample: 
-  const { data } = props
-  */
-
-  // [ADDITIONAL_HOOKS]
-  /*
-  code sample: 
-  const firestore = useFirestore()
-  */
-
-  // [COMPONENT_STATE_HOOKS]
-  /*
-  code sample:
-  const singleton = useRef(true) // references also put here
-  const [state, setState] = useState({})
-  */
-
-  // [HELPER_FUNCTIONS]
-
-  // [COMPUTED_PROPERTIES]
-  /* 
-    code sample: 
-    const userDisplayName = user.firstName + user.lastName
-  */
-
-  // [USE_EFFECTS]
+  const { data, columns } = props
 
   // [TEMPLATE]
-  return <div>UserSimpleTable</div>
+  return <Table columns={columns} dataSource={data} pagination={false} />
 }
 
 // [PROPTYPES]
-UserSimpleTable.propTypes = {}
+UserSimpleTable.propTypes = {
+  data: PropTypes.array.isRequired,
+  columns: PropTypes.array.isRequired
+}
 
 export default UserSimpleTable
