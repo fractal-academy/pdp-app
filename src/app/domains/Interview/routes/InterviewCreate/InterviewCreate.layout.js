0f4273c81c8d7314a4d6f450e7b33f03067a4cad
Olena <olenashevchuk0@gmail.com>
2021-03-10 19:40:15 +0200

    added interview simple list and interview create

--- src/app/domains/Interview/routes/InterviewCreate/InterviewCreate.layout.js
+++ src/app/domains/Interview/routes/InterviewCreate/InterviewCreate.layout.js
@@ -1,47 +1,71 @@
-import PropTypes from 'prop-types'
+import { Button } from 'antd'
+import { Row, Col, Back, HeadingPrimary } from 'antd-styled'
+import { InterviewSimpleList } from 'domains/Interview/components/lists'
+import { InterviewSimpleForm } from 'domains/Interview/components/forms'
+import { useState } from 'react'
+import _ from 'lodash'
 
 /**
  * @info InterviewCreate (05 Mar 2021) // CREATION DATE
  *
  * @comment InterviewCreate - React component.
  *
- * @since 05 Mar 2021 ( v.0.0.1 ) // LAST-EDIT DATE
+ * @since 10 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const InterviewCreate = (props) => {
-  // [INTERFACES]
-  /*
-  code sample: 
-  const { data } = props
-  */
-
-  // [ADDITIONAL_HOOKS]
-  /*
-  code sample: 
-  const firestore = useFirestore()
-  */
-
   // [COMPONENT_STATE_HOOKS]
-  /*
-  code sample:
-  const singleton = useRef(true) // references also put here
-  const [state, setState] = useState({})
-  */
+  const [questions, setQuestions] = useState([])
+  const [editQuestion, setEditQuestion] = useState(false)
 
   // [HELPER_FUNCTIONS]
+  const onSubmit = (value) => {
+    value && setQuestions([...questions, value])
+    setEditQuestion('')
+  }
 
-  // [COMPUTED_PROPERTIES]
-  /* 
-    code sample: 
-    const userDisplayName = user.firstName + user.lastName
-  */
-
-  // [USE_EFFECTS]
+  const onDeleteQuestion = (idx) => {
+    const newQuestions = _.remove(questions, (item, index) => {
+      return index !== idx
+    })
+    setQuestions(newQuestions)
+  }
 
   // [TEMPLATE]
-  return <div>InterviewCreate</div>
+  return (
+    <Row>
+      <Col span={24}>
+        <Row gutter={[8]} mb={3} justify="space-between">
+          <Col>
+            <Back size="large">Back</Back>
+          </Col>
+          <Col>
+            <Button size="large" type="primary">
+              Next
+            </Button>
+          </Col>
+        </Row>
+        <HeadingPrimary title="Create questions for interview" />
+        <Row gutter={[8, 16]} justify="center">
+          <Col span={16}>
+            <InterviewSimpleForm
+              onSubmit={onSubmit}
+              editQuestion={editQuestion}
+            />
+          </Col>
+          <Col span={16}>
+            <InterviewSimpleList
+              setQuestions={setQuestions}
+              questions={questions}
+              onDeleteQuestion={onDeleteQuestion}
+            />
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  )
 }
 
 // [PROPTYPES]
