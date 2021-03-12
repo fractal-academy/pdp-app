259ab76a524ff4e2c4fde768cce6839504241e82
Olena <olenashevchuk0@gmail.com>
2021-03-12 23:18:09 +0200

    added Spinner to components

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import { Navigation } from './Navigation'
 import { Sidebar } from './Sidebar'
 import { AccessDenied } from './AccessDenied'
 import { NotFoundPath } from './NotFoundPath'
-export { Navigation, Sidebar, AccessDenied, NotFoundPath }
+import { Spinner } from './Spinner'
+export { Navigation, Sidebar, AccessDenied, NotFoundPath, Spinner }
