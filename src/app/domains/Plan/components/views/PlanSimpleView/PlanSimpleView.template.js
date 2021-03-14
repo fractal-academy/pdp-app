5eb508e8758417e8232b045ea720ed5b6a82fbf8
Olena <olenashevchuk0@gmail.com>
2021-03-14 23:59:27 +0200

    created PlanSmplView

--- src/app/domains/Plan/components/views/PlanSimpleView/PlanSimpleView.template.js
+++ src/app/domains/Plan/components/views/PlanSimpleView/PlanSimpleView.template.js
@@ -1,50 +1,42 @@
 import PropTypes from 'prop-types'
-
+import firestore from '~/services/Firebase/firestore/index'
+import { Typography } from 'antd'
+import { Box } from 'antd-styled'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+import { COLLECTIONS } from 'app/constants'
+const { Title, Text } = Typography
 /**
  * @info PlanSimpleView (05 Mar 2021) // CREATION DATE
  *
  * @comment PlanSimpleView - React component.
  *
- * @since 05 Mar 2021 ( v.0.0.1 ) // LAST-EDIT DATE
+ * @since 14 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const PlanSimpleView = (props) => {
   // [INTERFACES]
-  /*
-  code sample: 
-  const { data } = props
-  */
+  const { planId, ...rest } = props
 
   // [ADDITIONAL_HOOKS]
-  /*
-  code sample: 
-  const firestore = useFirestore()
-  */
-
-  // [COMPONENT_STATE_HOOKS]
-  /*
-  code sample:
-  const singleton = useRef(true) // references also put here
-  const [state, setState] = useState({})
-  */
-
-  // [HELPER_FUNCTIONS]
-
-  // [COMPUTED_PROPERTIES]
-  /* 
-    code sample: 
-    const userDisplayName = user.firstName + user.lastName
-  */
-
-  // [USE_EFFECTS]
+  const [plan, loading] = useDocumentData(
+    firestore.collection(COLLECTIONS.PLANS).doc(planId)
+  )
 
   // [TEMPLATE]
-  return <div>PlanSimpleView</div>
+  if (loading) return <Text type="secondary">loading...</Text>
+
+  return (
+    <Box display="inline-block" {...rest}>
+      <Title level={3}>{plan.name}</Title>
+    </Box>
+  )
 }
 
 // [PROPTYPES]
-PlanSimpleView.propTypes = {}
+PlanSimpleView.propTypes = {
+  planId: PropTypes.string.isRequired
+}
 
 export default PlanSimpleView
