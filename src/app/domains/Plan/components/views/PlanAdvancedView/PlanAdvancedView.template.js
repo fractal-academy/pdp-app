6e6483ee861e4cc3fb7e3aca82051799e56c8b6f
Olena <olenashevchuk0@gmail.com>
2021-03-14 23:58:55 +0200

    created PlanSmplList and PlanAvdView

--- src/app/domains/Plan/components/views/PlanAdvancedView/PlanAdvancedView.template.js
+++ src/app/domains/Plan/components/views/PlanAdvancedView/PlanAdvancedView.template.js
@@ -1,52 +1,55 @@
 import PropTypes from 'prop-types'
-
+import { Box } from 'antd-styled'
+import { Typography } from 'antd'
+import { PlanSimpleView } from 'domains/Plan/components/views'
+import { Status } from '~/components'
+import { FieldTimeOutlined } from '@ant-design/icons'
+import firestore from '~/services/Firebase/firestore/index'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+import { COLLECTIONS } from 'app/constants'
+import moment from 'moment'
+const { Text } = Typography
 /**
  * @info PlanAdvancedView (14 Mar 2021) // CREATION DATE
  *
  * @comment PlanAdvancedView - React component.
  *
- * @since 14 Mar 2021 ( v.0.0.1 ) // LAST-EDIT DATE
+ * @since 14 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const PlanAdvancedView = (props) => {
   // [INTERFACES]
-  /*
-  code sample: 
-  const { data } = props
-  */
+  const { planId } = props
 
   // [ADDITIONAL_HOOKS]
-  /*
-  code sample: 
-  const firestore = useFirestore()
-  */
-
-  // [COMPONENT_STATE_HOOKS]
-  /*
-  code sample:
-  const singleton = useRef(true) // references also put here
-  const [state, setState] = useState({})
-  */
-
-  // [HELPER_FUNCTIONS]
-
-  // [COMPUTED_PROPERTIES]
-  /* 
-    code sample: 
-    const userDisplayName = user.firstName + user.lastName
-  */
-
-  // [USE_EFFECTS]
+  const [plan, loading] = useDocumentData(
+    firestore.collection(COLLECTIONS.PLANS).doc(planId)
+  )
 
   // [TEMPLATE]
-  return <div>PlanAdvancedView</div>
+  if (loading) return <Text type="secondary">loading...</Text>
+
+  return (
+    <Box display="flex" justifyContent="space-between">
+      <Box display="flex" alignItems="center">
+        <PlanSimpleView planId={plan.id} mr={4} />
+        <Status status={plan.status} />
+      </Box>
+      <Box>
+        <FieldTimeOutlined />
+        <Box ml={1} display="inline-block">
+          <Text>{moment(plan?.period?.end).format('DD.MM.YYYY')}</Text>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 // [PROPTYPES]
 PlanAdvancedView.propTypes = {
-  props: PropTypes.object
+  planId: PropTypes.string.isRequired
 }
 
 export default PlanAdvancedView
