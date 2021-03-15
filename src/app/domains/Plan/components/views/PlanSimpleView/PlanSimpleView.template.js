38dcdf6ee2697f8b4d35b899ba289eade7eea390
Olena <olenashevchuk0@gmail.com>
2021-03-15 11:04:30 +0200

    fixed PlanSmplView (added prop - name) and deleted request to firebase

--- src/app/domains/Plan/components/views/PlanSimpleView/PlanSimpleView.template.js
+++ src/app/domains/Plan/components/views/PlanSimpleView/PlanSimpleView.template.js
@@ -1,42 +1,32 @@
 import PropTypes from 'prop-types'
-import firestore from '~/services/Firebase/firestore/index'
 import { Typography } from 'antd'
 import { Box } from 'antd-styled'
-import { useDocumentData } from 'react-firebase-hooks/firestore'
-import { COLLECTIONS } from 'app/constants'
-const { Title, Text } = Typography
+const { Title } = Typography
 /**
  * @info PlanSimpleView (05 Mar 2021) // CREATION DATE
  *
  * @comment PlanSimpleView - React component.
  *
- * @since 14 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
+ * @since 15 Mar 2021 ( v.0.0.3 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const PlanSimpleView = (props) => {
   // [INTERFACES]
-  const { planId, ...rest } = props
-
-  // [ADDITIONAL_HOOKS]
-  const [plan, loading] = useDocumentData(
-    firestore.collection(COLLECTIONS.PLANS).doc(planId)
-  )
+  const { name, ...rest } = props
 
   // [TEMPLATE]
-  if (loading) return <Text type="secondary">loading...</Text>
-
   return (
     <Box display="inline-block" {...rest}>
-      <Title level={3}>{plan.name}</Title>
+      <Title level={3}>{name}</Title>
     </Box>
   )
 }
 
 // [PROPTYPES]
 PlanSimpleView.propTypes = {
-  planId: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired
 }
 
 export default PlanSimpleView
