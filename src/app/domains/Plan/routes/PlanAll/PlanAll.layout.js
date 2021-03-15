0e9f95191769c9fcc96eae2cca944b0a289e351a
Olena <olenashevchuk0@gmail.com>
2021-03-15 11:14:39 +0200

    added PageWrapper to PlanAll

--- src/app/domains/Plan/routes/PlanAll/PlanAll.layout.js
+++ src/app/domains/Plan/routes/PlanAll/PlanAll.layout.js
@@ -1,17 +1,18 @@
 import { Content, Sider, Title } from 'antd-styled'
-
 import firestore from '~/services/Firebase/firestore/index'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { COLLECTIONS } from 'app/constants'
 import { PlanSimpleList } from 'domains/Plan/components/lists'
 import { Spinner } from '~/components'
 import { useSession } from 'contexts/Session/hooks'
+import { PageWrapper } from '~/components/HOC'
+import { useHistory } from 'react-router-dom'
 /**
  * @info PlanAll (05 Mar 2021) // CREATION DATE
  *
  * @comment PlanAll - React component.
  *
- * @since 14 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
+ * @since 15 Mar 2021 ( v.0.0.3 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
@@ -22,13 +23,21 @@ const PlanAll = () => {
   const [plans, loading] = useCollectionData(
     firestore.collection(COLLECTIONS.PLANS).where('id', 'in', session.planIds)
   )
+  const history = useHistory()
 
   // [TEMPLATE]
   return (
     <>
       <Content bg="#ffffff" paddingTop={4} paddingX={4}>
-        <Title>My plans</Title>
-        {loading ? <Spinner /> : <PlanSimpleList data={plans} />}
+        <PageWrapper
+          title="My plans"
+          backBtnLeft
+          titleProps={{ textAlign: 'left' }}
+          onBack={() => history.goBack()}
+          inlineHeader
+          fullWidth>
+          {loading ? <Spinner /> : <PlanSimpleList data={plans} />}
+        </PageWrapper>
       </Content>
       <Sider paddingTop={4} paddingX={4}>
         <Title textAlign="center" style={{ color: 'white' }}>
