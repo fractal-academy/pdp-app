a38fa65d75a82251c9cf317e0f0309abd1625015
Olena <olenashevchuk0@gmail.com>
2021-03-15 12:20:48 +0200

    foxed competenceSmplView

--- src/app/domains/Competence/components/views/CompetenceSimpleView/CompetenceSimpleView.template.js
+++ src/app/domains/Competence/components/views/CompetenceSimpleView/CompetenceSimpleView.template.js
@@ -1,37 +1,27 @@
 import PropTypes from 'prop-types'
 import { Typography } from 'antd'
-import firestore from '~/services/Firebase/firestore/index'
-import { useDocumentData } from 'react-firebase-hooks/firestore'
-import { COLLECTIONS } from 'app/constants'
-const { Text, Title } = Typography
+const { Title } = Typography
 /**
  * @info CompetenceSimpleView (07 Mar 2021) // CREATION DATE
  *
  * @comment CompetenceSimpleView - React component.
  *
- * @since 12 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
+ * @since 15 Mar 2021 ( v.0.0.3 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const CompetenceSimpleView = (props) => {
   // [INTERFACES]
-  const { competenceId } = props
-
-  // [ADDITIONAL_HOOKS]
-  const [competence, loading] = useDocumentData(
-    firestore.collection(COLLECTIONS.COMPETENCES).doc(competenceId)
-  )
+  const { name } = props
 
   // [TEMPLATE]
-  if (loading) return <Text type="secondary">loading...</Text>
-
-  return <Title level={3}>{competence.name}</Title>
+  return <Title level={3}>{name}</Title>
 }
 
 // [PROPTYPES]
 CompetenceSimpleView.propTypes = {
-  competenceId: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired
 }
 
 export default CompetenceSimpleView
