915dd10e1e43096db094f0f7b11e2962304bde1a
Maxim <maxim.makarov14@gmail.com>
2021-03-15 14:57:42 +0200

    Generated component for todo list

---
+++ src/app/domains/Todo/components/combined/views/TodoViewWIthActions/index.js
@@ -0,0 +1,1 @@
+export { default as TodoViewWIthActions } from './TodoViewWIthActions.template'
--- src/app/domains/Todo/components/views/TodoSimpleView/__tests__/TodoSimpleView.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import TodoSimpleView from '../TodoSimpleView.template'
-
-test('Render TodoSimpleView test.', () => {
-  render(<TodoSimpleView />)
-})
