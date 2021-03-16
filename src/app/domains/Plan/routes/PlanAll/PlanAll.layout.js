363cae8d674f233f792b39aa260c98f9f329e891
Olena <olenashevchuk0@gmail.com>
2021-03-16 23:30:10 +0200

    fixed layout in PlanAll, added request to db

--- src/app/domains/Plan/routes/PlanAll/PlanAll.layout.js
+++ src/app/domains/Plan/routes/PlanAll/PlanAll.layout.js
@@ -1,27 +1,39 @@
 import { Content, Sider, Title } from 'antd-styled'
+import { List } from 'antd'
 import firestore from '~/services/Firebase/firestore/index'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
+import {
+  useCollectionData,
+  useDocumentData
+} from 'react-firebase-hooks/firestore'
 import { COLLECTIONS } from 'app/constants'
 import { PlanSimpleList } from 'domains/Plan/components/lists'
+import { TodoAdvancedList } from 'domains/Todo/components/lists'
 import { Spinner } from '~/components'
 import { useSession } from 'contexts/Session/hooks'
 import { PageWrapper } from '~/components/HOC'
 import { useHistory } from 'react-router-dom'
+import { useState } from 'react'
 /**
  * @info PlanAll (05 Mar 2021) // CREATION DATE
  *
  * @comment PlanAll - React component.
  *
- * @since 15 Mar 2021 ( v.0.0.3 ) // LAST-EDIT DATE
+ * @since 16 Mar 2021 ( v.0.0.4 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const PlanAll = () => {
+  // [COMPONENT_STATE_HOOKS]
+  const [activePlanId, setActivePlanId] = useState('')
+
   // [ADDITIONAL_HOOKS]
   const session = useSession()
   const [plans, loading] = useCollectionData(
     firestore.collection(COLLECTIONS.PLANS).where('id', 'in', session.planIds)
+  )
+  const [activePlan, loadingActivePlan] = useDocumentData(
+    activePlanId && firestore.collection(COLLECTIONS.PLANS).doc(activePlanId)
   )
   const history = useHistory()
 
@@ -36,13 +48,27 @@ const PlanAll = () => {
           onBack={() => history.goBack()}
           inlineHeader
           fullWidth>
-          {loading ? <Spinner /> : <PlanSimpleList data={plans} />}
+          {loading ? (
+            <Spinner />
+          ) : (
+            <PlanSimpleList plans={plans} setActivePlanId={setActivePlanId} />
+          )}
         </PageWrapper>
       </Content>
-      <Sider paddingTop={4} paddingX={4}>
+      <Sider paddingTop={4} paddingX={4} width="25%">
         <Title textAlign="center" style={{ color: 'white' }}>
           Todo
         </Title>
+        {loadingActivePlan ? (
+          <Spinner />
+        ) : activePlan ? (
+          <TodoAdvancedList plan={activePlan} activePlanId={activePlanId} />
+        ) : (
+          <List
+            dataSource={plans}
+            renderItem={(plan) => <TodoAdvancedList plan={plan} />}
+          />
+        )}
       </Sider>
     </>
   )
