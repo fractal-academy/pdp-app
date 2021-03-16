1e57559bd8a68d6349104a4a2220e590ac0d3a6d
Olena <olenashevchuk0@gmail.com>
2021-03-16 23:33:28 +0200

    created layout and logic in TodoViewWithActions

--- src/app/domains/Todo/components/combined/views/TodoViewWIthActions/TodoViewWIthActions.template.js
+++ src/app/domains/Todo/components/combined/views/TodoViewWIthActions/TodoViewWIthActions.template.js
@@ -1,47 +1,87 @@
 import PropTypes from 'prop-types'
-import { Checkbox } from 'antd'
-
+import { useState } from 'react'
+import { Checkbox, Space } from 'antd'
+import { Paragraph, Box, Remove } from 'antd-styled'
+import { DeleteOutlined } from '@ant-design/icons'
+import firestore from '~/services/Firebase/firestore/index'
 /**
  * @info TodoViewWIthActions (15 Mar 2021) // CREATION DATE
  *
  * @comment TodoViewWIthActions - React component.
  *
- * @since 15 Mar 2021 ( v.0.0.1 ) // LAST-EDIT DATE
+ * @since 16 Mar 2021 ( v.0.0.3 ) // LAST-EDIT DATE
  *
  * @return {React.FC}
  */
 
 const TodoViewWIthActions = (props) => {
   // [INTERFACES]
-  const { technology } = props
-  const options = []
-  const defaultValue = []
+  const { todo, refTodoDocument } = props
+
+  // [COMPONENT_STATE_HOOKS]
+  const [isEdit, setIsEdit] = useState(false)
 
   // [HELPER_FUNCTIONS]
-  const getOptions = () =>
-    Object.keys(technology.todoIds).forEach((todoId) => {
-      options.push({
-        label: technology.todoIds[todoId].name,
-        value: technology.todoIds[todoId].id
-      })
-      technology.todoIds[todoId].isDone &&
-        defaultValue.push(technology.todoIds[todoId].id)
+  const onChangeDone = () => {
+    firestore.doc(refTodoDocument).set({
+      ...todo,
+      isDone: !todo.isDone
     })
-  getOptions()
+  }
+
+  const onEditTodo = (name) => {
+    firestore.doc(refTodoDocument).set({
+      ...todo,
+      name: name
+    })
+    setIsEdit(false)
+  }
+
+  const onDeleteTodo = () => {
+    firestore.doc(refTodoDocument).delete()
+  }
 
   // [TEMPLATE]
   return (
-    <Checkbox.Group
-      options={options && options}
-      defaultValue={defaultValue && defaultValue}
-    />
+    <>
+      {todo && (
+        <Box display="flex" justifyContent="space-between" mb={2}>
+          <Space>
+            {!isEdit && (
+              <Checkbox checked={todo.isDone} onChange={onChangeDone} />
+            )}
+            <Paragraph
+              display="inline-block"
+              color="white"
+              onClick={() => !todo.readOnly && setIsEdit(true)}
+              editable={{
+                icon: <></>,
+                editing: isEdit ? true : false,
+                onChange: onEditTodo
+              }}>
+              {todo.name}
+            </Paragraph>
+          </Space>
+          {!todo.readOnly && (
+            <Remove
+              shape="default"
+              tooltip="Remove"
+              type="text"
+              icon={<DeleteOutlined />}
+              onSubmit={onDeleteTodo}
+            />
+          )}
+        </Box>
+      )}
+    </>
   )
 }
 
 // [PROPTYPES]
 TodoViewWIthActions.propTypes = {
-  planId: PropTypes.string.isRequired,
-  technologyId: PropTypes.string.isRequired
+  todo: PropTypes.string.isRequired,
+  technology: PropTypes.string.isRequired,
+  refTodoDocument: PropTypes.string.isRequired
 }
 
 export default TodoViewWIthActions
---
+++ src/app/domains/Todo/components/lists/TodoAdvancesList/index.js
@@ -0,0 +1,1 @@
+export { default as TodoAdvancedList } from './TodoAdvancedList.template'
