cf3b0397f46818503e88dea338f4870d10bbad16
Maxim <maxim.makarov14@gmail.com>
2021-03-17 18:43:46 +0200

    Fixed routing, fixed technology list error

--- src/app/index.js
+++ src/app/index.js
@@ -3,13 +3,14 @@ import { Layout } from 'antd-styled'
 import { Navigation } from '~/components'
 import { ROUTES_VALUES, ROUTE_PATHS } from 'app/constants'
 import { useSession } from 'contexts/Session/hooks'
+import { SessionLogin, SessionRegister } from 'domains/Session/routes'
 
 /**
  * @info App (05 Mar 2021) // CREATION DATE
  *
  * @comment App - React component.
  *
- * @since 11 Mar 2021 ( v.0.0.4 ) // LAST-EDIT DATE
+ * @since 17 Mar 2021 ( v.0.0.5 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
@@ -20,24 +21,27 @@ const App = () => {
 
   // [TEMPLATE]
   return (
-    <Layout>
-      <Navigation />
-      <Switch>
-        <Redirect
-          from="/"
-          to={ROUTE_PATHS.START_PAGE_MAP[session.role]}
-          exact
-        />
-        {ROUTES_VALUES.map((route) =>
-          route.Component ? (
-            <route.Component key={route.Component.name} />
-          ) : (
-            <Route key={route.path} {...route} />
-          )
-        )}
-        <Redirect to={ROUTE_PATHS.NOT_FOUND_PATH} />
-      </Switch>
-    </Layout>
+    <Switch>
+      <Redirect from="/" to={ROUTE_PATHS.START_PAGE_MAP[session.role]} exact />
+      <Route component={SessionLogin} path={ROUTE_PATHS.SESSION_LOGIN} />
+      <Route
+        component={SessionRegister}
+        path={ROUTE_PATHS.SESSION_REGISTRATION}
+      />
+      <Layout>
+        <Navigation />
+        <Switch>
+          {ROUTES_VALUES.map((route) =>
+            route.Component ? (
+              <route.Component key={route.Component.name} />
+            ) : (
+              <Route key={route.path} {...route} />
+            )
+          )}
+        </Switch>
+      </Layout>
+      <Redirect to={ROUTE_PATHS.NOT_FOUND_PATH} />
+    </Switch>
   )
 }
 
