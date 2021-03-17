7aae534d22bac3f1ddae73602e4b31a05771441b
Olena <olenashevchuk0@gmail.com>
2021-03-17 19:10:37 +0200

    added sessionSmplForm, Login and Register layout

--- src/app/domains/Session/components/forms/SessionSimpleForm/SessionSimpleForm.template.js
+++ src/app/domains/Session/components/forms/SessionSimpleForm/SessionSimpleForm.template.js
@@ -1,27 +1,23 @@
 import PropTypes from 'prop-types'
+import { Form, Input, Button, Space } from 'antd'
+import { Box } from 'antd-styled'
 
 /**
  * @info SessionSimpleForm (05 Mar 2021) // CREATION DATE
  *
  * @comment SessionSimpleForm - React component.
  *
- * @since 05 Mar 2021 ( v.0.0.1 ) // LAST-EDIT DATE
+ * @since 17 Mar 2021 ( v.0.0.1 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const SessionSimpleForm = (props) => {
   // [INTERFACES]
-  /*
-  code sample: 
-  const { data } = props
-  */
+  const { register } = props
 
   // [ADDITIONAL_HOOKS]
-  /*
-  code sample: 
-  const firestore = useFirestore()
-  */
+  const [form] = Form.useForm()
 
   // [COMPONENT_STATE_HOOKS]
   /*
@@ -31,7 +27,10 @@ const SessionSimpleForm = (props) => {
   */
 
   // [HELPER_FUNCTIONS]
-
+  const onFinish = (values) => {}
+  const onReset = () => {
+    form.resetFields()
+  }
   // [COMPUTED_PROPERTIES]
   /* 
     code sample: 
@@ -41,10 +40,46 @@ const SessionSimpleForm = (props) => {
   // [USE_EFFECTS]
 
   // [TEMPLATE]
-  return <div>SessionSimpleForm</div>
+  return (
+    <Form size="large" onFinish={onFinish} form={form}>
+      <Form.Item
+        name="email"
+        rules={[{ required: true, message: 'Please input your email' }]}>
+        <Input placeholder="Enter email" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your password' }]}>
+        <Input placeholder="Enter password" />
+      </Form.Item>
+      {register && (
+        <Form.Item
+          name="confirm"
+          rules={[
+            { required: true, message: 'Please input your password again' }
+          ]}>
+          <Input placeholder="Enter password again" />
+        </Form.Item>
+      )}
+      <Form.Item>
+        <Box display="flex" justifyContent="center">
+          <Space size="large">
+            <Button type="primary" htmlType="submit">
+              {register ? 'Sign in' : 'Log in'}
+            </Button>
+            <Button htmlType="button" onClick={onReset}>
+              Reset
+            </Button>
+          </Space>
+        </Box>
+      </Form.Item>
+    </Form>
+  )
 }
 
 // [PROPTYPES]
-SessionSimpleForm.propTypes = {}
+SessionSimpleForm.propTypes = {
+  register: PropTypes.bool
+}
 
 export default SessionSimpleForm
