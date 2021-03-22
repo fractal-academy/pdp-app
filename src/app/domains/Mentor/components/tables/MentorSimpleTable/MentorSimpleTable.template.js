ccac5d7c679783e2a0ee7e854f83dd291a410a04
Olena <olenashevchuk0@gmail.com>
2021-03-22 22:50:47 +0200

    added "merge" data student and mentor table

--- src/app/domains/Mentor/components/tables/MentorSimpleTable/MentorSimpleTable.template.js
+++ src/app/domains/Mentor/components/tables/MentorSimpleTable/MentorSimpleTable.template.js
@@ -1,18 +1,21 @@
+import { useEffect, useState } from 'react'
 import { Table, Typography } from 'antd'
+import { useHistory, generatePath } from 'react-router-dom'
 import { UserSimpleView } from 'domains/User/components/views'
 import { CompanySimpleView } from 'domains/Company/components/views'
 import { CompetenceSimpleView } from 'domains/Competence/components/views'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import firestore from '~/services/Firebase/firestore'
 import { Spinner } from '~/components'
-import { COLLECTIONS } from 'app/constants'
+import { COLLECTIONS, ROUTE_PATHS } from 'app/constants'
+import _ from 'lodash'
 const { Text } = Typography
 /**
  * @info MentorSimpleTable (22 Mar 2021) // CREATION DATE
  *
  * @comment MentorSimpleTable - React component.
  *
- * @since 22 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
+ * @since 22 Mar 2021 ( v.0.0.3 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
@@ -54,14 +57,55 @@ let columns = [
 
 const MentorSimpleTable = () => {
   // [ADDITIONAL_HOOKS]
+  const history = useHistory()
   const [mentorsData, loading] = useCollectionData(
     firestore.collection(COLLECTIONS.MENTORS)
   )
 
-  // [TEMPLATE]
-  if (loading) return <Spinner />
+  // [COMPONENT_STATE_HOOKS]
+  const [userData, setUserData] = useState([])
+  const [usersLoading, setUsersLoading] = useState(true)
 
-  return <Table columns={columns} dataSource={mentorsData} pagination={false} />
+  // [USE_EFFECTS]
+  useEffect(() => {
+    if (mentorsData) {
+      const fetchUser = async () => {
+        setUsersLoading(true)
+        const usersSnapshot = await firestore
+          .collection(COLLECTIONS.USERS)
+          .where('role', '==', 'mentor')
+          .get()
+
+        const users = usersSnapshot.docs.map((snapshot) => snapshot.data())
+
+        setUserData(
+          Object.values(
+            _.merge(_.keyBy(users, 'id'), _.keyBy(mentorsData, 'userId'))
+          )
+        )
+        setUsersLoading(false)
+      }
+      fetchUser()
+    }
+  }, [mentorsData])
+
+  // [TEMPLATE]
+  if (loading && usersLoading) return <Spinner />
+
+  return (
+    <Table
+      onRow={(record) => ({
+        onClick: () =>
+          history.push(generatePath(ROUTE_PATHS.USER_SHOW, { id: record.id })),
+        onMouseEnter: (e) => {
+          e.target.style.cursor = 'pointer'
+        }
+      })}
+      columns={columns}
+      dataSource={userData}
+      pagination={false}
+    />
+  )
 }
 
 export default MentorSimpleTable
--- src/app/domains/Student/components/tables/StudentSimpleTable/StudentSimpleTable.template.js
+++ src/app/domains/Student/components/tables/StudentSimpleTable/StudentSimpleTable.template.js
@@ -1,4 +1,5 @@
-import { Table, Typography } from 'antd'
+import { useEffect, useState } from 'react'
+import { Button, Table, Typography } from 'antd'
 import { useHistory, generatePath } from 'react-router-dom'
 import { UserSimpleView } from 'domains/User/components/views'
 import { CompanySimpleView } from 'domains/Company/components/views'
@@ -6,6 +7,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { ROUTE_PATHS, COLLECTIONS } from 'app/constants'
 import firestore from '~/services/Firebase/firestore'
 import { Spinner } from '~/components'
+import _ from 'lodash'
 const { Text } = Typography
 
 /**
@@ -13,7 +15,7 @@ const { Text } = Typography
  *
  * @comment StudentSimpleTable - React component.
  *
- * @since 22 Mar 2021 ( v.0.0.5 ) // LAST-EDIT DATE
+ * @since 22 Mar 2021 ( v.0.0.6 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
@@ -32,7 +34,7 @@ let columns = [
   },
   {
     title: 'Company',
-    dataIndex: 'company',
+    dataIndex: 'companyId',
     key: 'company',
     render: (text, data) =>
       data.companyId ? (
@@ -40,20 +42,65 @@ let columns = [
       ) : (
         <Text type="secondary">None</Text>
       )
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    dataIndex: '',
+    render: (text, data) => <CreatePlanButton studentId={data.id} />
   }
 ]
+
+const CreatePlanButton = (props) => {
+  const { studentId } = props
+  const history = useHistory()
+  return (
+    <Button
+      type="primary"
+      onClick={() =>
+        history.push(ROUTE_PATHS.PLAN_CREATE, { student: { id: studentId } })
+      }>
+      Create plan
+    </Button>
+  )
+}
 
 const StudentSimpleTable = () => {
   // [ADDITIONAL_HOOKS]
   const history = useHistory()
-
-  // [ADDITIONAL_HOOKS]
   const [studentsData, loading] = useCollectionData(
     firestore.collection(COLLECTIONS.STUDENTS)
   )
 
+  // [COMPONENT_STATE_HOOKS]
+  const [userData, setUserData] = useState([])
+  const [usersLoading, setUsersLoading] = useState(true)
+
+  // [USE_EFFECTS]
+  useEffect(() => {
+    if (studentsData) {
+      const fetchUser = async () => {
+        setUsersLoading(true)
+        const usersSnapshot = await firestore
+          .collection(COLLECTIONS.USERS)
+          .where('role', '==', 'student')
+          .get()
+
+        const users = usersSnapshot.docs.map((snapshot) => snapshot.data())
+
+        setUserData(
+          Object.values(
+            _.merge(_.keyBy(users, 'id'), _.keyBy(studentsData, 'userId'))
+          )
+        )
+        setUsersLoading(false)
+      }
+      fetchUser()
+    }
+  }, [studentsData])
+
   // [TEMPLATE]
-  if (loading) return <Spinner />
+  if (loading && usersLoading) return <Spinner />
 
   return (
     <Table
@@ -65,7 +112,7 @@ const StudentSimpleTable = () => {
         }
       })}
       columns={columns}
-      dataSource={studentsData}
+      dataSource={userData}
       pagination={false}
     />
   )
