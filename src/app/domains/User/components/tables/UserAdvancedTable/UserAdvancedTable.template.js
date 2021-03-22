f080d563371018b5b2347bdd12452614fb3f94ef
Olena <olenashevchuk0@gmail.com>
2021-03-22 19:21:53 +0200

    added new components to UserAdvTable

--- src/app/domains/User/components/tables/UserAdvancedTable/UserAdvancedTable.template.js
+++ src/app/domains/User/components/tables/UserAdvancedTable/UserAdvancedTable.template.js
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import { UserSimpleTable } from 'domains/User/components/tables'
-import { ROLES } from '~/constants'
 import { Tabs } from 'antd'
+import { UserSimpleTable } from 'domains/User/components/tables'
+import { StudentSimpleTable } from 'domains/Student/components/tables'
+import { MentorSimpleTable } from 'app/domains/Mentor/components/tables'
 const { TabPane } = Tabs
 /**
  * @info UserAdvancedTable (05 Mar 2021) // CREATION DATE
  *
  * @comment UserAdvancedTable - React component.
  *
- * @since 22 Mar 2021 ( v.0.0.3 ) // LAST-EDIT DATE
+ * @since 22 Mar 2021 ( v.0.0.4 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const TABS = [
   {
-    tab: { title: 'All', viewFor: '' }
+    tab: { title: 'All', Component: <UserSimpleTable /> }
   },
   {
-    tab: { title: 'Mentors', viewFor: ROLES.MENTOR }
+    tab: { title: 'Mentors', Component: <MentorSimpleTable /> }
   },
   {
-    tab: { title: 'Students', viewFor: ROLES.STUDENT }
+    tab: { title: 'Students', Component: <StudentSimpleTable /> }
   }
 ]
 
@@ -29,19 +29,12 @@ const UserAdvancedTable = (props) => {
   // [INTERFACES]
   const { ...rest } = props
 
-  // [COMPONENT_STATE_HOOKS]
-  const [viewFor, setViewFor] = useState()
-
   // [TEMPLATE]
   return (
-    <Tabs
-      defaultActiveKey={TABS[0].tab.title}
-      size="large"
-      onChange={(tab) => setViewFor(tab)}
-      {...rest}>
+    <Tabs defaultActiveKey={TABS[0].tab.title} size="large" {...rest}>
       {TABS.map((item) => (
-        <TabPane tab={item.tab.title} key={item.tab.viewFor}>
-          <UserSimpleTable viewFor={viewFor} />
+        <TabPane tab={item.tab.title} key={item.tab.title}>
+          {item.tab.Component}
         </TabPane>
       ))}
     </Tabs>
