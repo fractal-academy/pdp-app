d60e88fb74497bcb65836cf9c351fce632e6989b
Olena <olenashevchuk0@gmail.com>
2021-03-22 19:17:10 +0200

    fixed userShow and  All

--- src/app/domains/User/routes/UserAll/UserAll.layout.js
+++ src/app/domains/User/routes/UserAll/UserAll.layout.js
@@ -1,23 +1,26 @@
 import { UserAdvancedTable } from 'domains/User/components/tables'
 import { PageWrapper } from '~/components/HOC'
-import { Button } from 'antd'
 /**
  * @info UserAll (05 Mar 2021) // CREATION DATE
  *
  * @comment UserAll - React component.
  *
- * @since 22 Mar 2021 ( v.0.0.3 ) // LAST-EDIT DATE
+ * @since 22 Mar 2021 ( v.0.0.4 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
 
 const UserAll = () => {
   // [TEMPLATE]
-  const AddButton = <Button type="primary">Add user</Button>
 
   return (
-    <PageWrapper title="Users" fullWidth inlineHeader>
-      <UserAdvancedTable tabBarExtraContent={{ right: AddButton }} />
+    <PageWrapper
+      title="Users"
+      fullWidth
+      inlineHeader
+      nextBtnProps={{ text: 'Add user' }}
+      onNext={() => console.log()}>
+      <UserAdvancedTable />
     </PageWrapper>
   )
 }
