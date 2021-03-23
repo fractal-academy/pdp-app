720ed74646e4a2e1eb07c59eb41d8cf417a9937c
Maxim <maxim.makarov14@gmail.com>
2021-03-23 13:35:32 +0200

    Generated component for user edit

---
+++ src/app/domains/User/components/combined/modals/UserModalWithForm/UserModalWithForm.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserModalWithForm from './UserModalWithForm.template'
+
+const metadata = {
+  title: 'app/domains/User/components/combined/modals/UserModalWithForm',
+  component: UserModalWithForm
+}
+export default metadata
+
+export const UserModalWithFormStory = (args) => <UserModalWithForm {...args} />
+
+UserModalWithFormStory.args = {}
---
+++ src/app/domains/User/components/combined/modals/UserModalWithForm/__tests__/UserModalWithForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import UserModalWithForm from '../UserModalWithForm.template'
+
+test('Render UserModalWithForm test.', () => {
+  render(<UserModalWithForm />)
+})
