4f9996b7f8b883e1e701e46072758f0e0a2801f6
Maxim <maxim.makarov14@gmail.com>
2021-03-25 08:44:28 +0200

    Implemented level cascader & refactored technology create component

---
+++ src/app/domains/Level/components/combined/cascaders/LevelSimpleCascader/LevelSimpleCascader.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LevelSimpleCascader from './LevelSimpleCascader.template'
+
+const metadata = {
+  title: 'app/domains/Level/components/combined/cascaders/LevelSimpleCascader',
+  component: LevelSimpleCascader
+}
+export default metadata
+
+export const LevelSimpleCascaderStory = (args) => (
+  <LevelSimpleCascader {...args} />
+)
+
+LevelSimpleCascaderStory.args = {}
