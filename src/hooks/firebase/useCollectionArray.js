bdba750cbfc91b63cdcbc54858673b886a76883d
Olena <olenashevchuk0@gmail.com>
2021-03-30 21:52:45 +0300

    added hook for array

---
+++ src/hooks/firebase/useCollectionArray.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import { getCollectionRef } from '~/services/Firebase/firestore'
+
+const useCollectionArray = (collectionPath, arrayIds = []) => {
+  const [loading, setLoading] = useState(arrayIds.length ? true : false)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      const buf = []
+      for (const id of arrayIds) {
+        try {
+          const docData = await getCollectionRef(collectionPath).doc(id).get()
+          console.log(docData)
+          buf.push(docData.data())
+          setData(buf)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      setLoading(false)
+    }
+
+    arrayIds.length && fetchData()
+  }, [])
+  console.log(data)
+  return [data, loading]
+}
+
+export default useCollectionArray
