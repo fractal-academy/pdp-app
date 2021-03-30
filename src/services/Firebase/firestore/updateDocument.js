c0c65f850dc5a04b859324ecede28c25ce5b4c46
Maxim <maxim.makarov14@gmail.com>
2021-03-30 16:12:37 +0300

    Extended update method in firestore service

--- src/services/Firebase/firestore/updateDocument.js
+++ src/services/Firebase/firestore/updateDocument.js
@@ -3,16 +3,20 @@ import firestore from '~/services/Firebase/firestore'
 /**
  * @info updateDocument (19 Mar 2021) // CREATION DATE
  *
- * @since 19 Mar 2021 ( v.0.0.1 ) // LAST-EDIT DATE
+ * @since 30 Mar 2021 ( v.0.0.2 ) // LAST-EDIT DATE
  *
- * @param {string}     path     Path to a collection.
- * @param {string}     id     	Path to a document.
- * @param {object}     data     Data for the document.
+ * @param {string}              path        Path to a collection.
+ * @param {string}              id     	    Path to a document.
+ * @param {object}              data        Data for the document.
+ * @param {{merge:boolean}}     [options]     An object to configure the method behavior.
  *
  * @return {Promise<void>}
  */
 
-const updateDocument = (path, id, data) => {
+const updateDocument = (path, id, data, options) => {
+  if (options?.merge) {
+    return firestore.collection(path).doc(id).set(data, options)
+  }
   return firestore.collection(path).doc(id).update(data)
 }
 
